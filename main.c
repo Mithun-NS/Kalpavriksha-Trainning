5c10b4c3bc8fb7582ef86cf5fd21319a25aeee2e
Mithun.NS <mithunsajin@gmail.com>
2025-10-29 12:05:07 +0530

    Refactor matrix operations and improve input validation

--- main.c
+++ main.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX_SIZE 10
+#define MIN_SIZE 2
+
+void generateMatrix(int n, int mat[n][n]);
+void printMatrix(int n, int mat[n][n], const char *message);
+void rotate90Clockwise(int n, int mat[n][n]);
+void applySmoothingFilter(int n, int mat[n][n]);
+
 int main() {
     int n;
-    printf("Enter matrix size (2-10): ");
+    printf("Enter matrix size (%d-%d): ",MIN_SIZE, MAX_SIZE);
     scanf("%d", &n);
 
-    if(n < 2 || n > 10) {
+    if(n < MIN_SIZE || n > MAX_SIZE) {
         printf("Invalid size.\n");
         return 0;
     }
@@ -15,20 +23,33 @@ int main() {
     int mat[n][n];
     int tempRow[n];
     srand(time(NULL));
+    generateMatrix(n, mat);
+    printMatrix(n, mat, "Original Randomly Generated Matrix:");
 
+    rotate90Clockwise(n, mat);
+    printMatrix(n, mat, "Matrix after 90-degree Clockwise Rotation:");
+
+    applySmoothingFilter(n, mat);
+    printMatrix(n, mat, "Matrix after Applying 3x3 Smoothing Filter:");
+
+    return 0;
+}
+void generateMatrix(int n, int mat[n][n]){
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             *(*(mat + i) + j) = rand() % 256;
         }
     }
-
-    printf("\nOriginal Randomly Generated Matrix:\n");
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            printf("%4d", *(*(mat + i) + j));
-        }
+}
+ void printMatrix(int n, int mat[n][n], const char *message) {
+    printf("\n%s\n", message);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            printf("%4d", *(*(mat + i)+j));
         printf("\n");
     }
+}
+void rotate90Clockwise(int n, int mat[n][n]){
     for(int i = 0; i < n; i++) {
         for(int j = i + 1; j < n; j++) {
             int *a = *(mat + i) + j;
@@ -38,7 +59,6 @@ int main() {
             *b = temp;
         }
     }
-
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n / 2; j++) {
             int *left = *(mat + i) + j;
@@ -48,14 +68,10 @@ int main() {
             *right = temp;
         }
     }
+}
 
-    printf("\nMatrix after 90-degree Clockwise Rotation:\n");
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            printf("%4d", *(*(mat + i) + j));
-        }
-        printf("\n");
-    }
+void applySmoothingFilter(int n, int mat[n][n]) {
+    int tempRow[n];
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             int sum = 0, count = 0;
@@ -73,14 +89,5 @@ int main() {
             *(*(mat + i) + j) = tempRow[j];
         }
     }
-
-    printf("\nMatrix after Applying 3x3 Smoothing Filter:\n");
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            printf("%4d", *(*(mat + i) + j));
-        }
-        printf("\n");
-    }
-
-    return 0;
+}
 }
